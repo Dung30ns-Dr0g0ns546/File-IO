cdc1c9d7aee54e2d6bdcdf956614b88d4c106a31
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-02-14 14:52:12 +0000

    commit

---
+++ fileIO.cpp
@@ -0,0 +1,25 @@
+int main(){
+	std::ifstream inFile;
+	std::stringstream converter;
+	std::string currentLine;
+
+	std::string sNumb1;
+	std::string sNumb2;
+	std::string text;
+
+	int numb1;
+	int numb2;
+	int text;
+
+	inFile.open("data.csv");
+	while(getLine(inFile, currentLine)){
+		converter.clear();
+		converter.str(currentLine);
+
+		getLine(converter, sNumb1, ",");
+		getLine(converter, sNumb2, ",");
+		getLine(converter, text);
+
+		converter.clear();
+		converter.str("");
+		converter << sNumb1 << "  " << sNumb2;
